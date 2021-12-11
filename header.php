96aa600d05fbcff0a00bc499f1c1d229e2866fac
root <root@constantconstipation.club>
2021-12-11 04:37:43 +0000

    First commit

---
+++ header.php
@@ -0,0 +1,15 @@
+<?php
+echo 
+'
+    <div class="site_title" id="site_title" name="site_title">
+        PASSWORD CREATOR
+    </div>
+
+    <ul>
+        <li><a class="active" href="search.php">Search</a></li>
+        <li><a href="delete.php">Delete</a></li>
+        <li><a href="about.php">About</a></li>
+    </ul>
+'
+; 
+?>
